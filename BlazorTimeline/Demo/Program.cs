0b1075631d7eae3997b770997fdbe15bf4aa3f05
Morasiu <moras2601@wp.pl>
2022-01-21 14:53:53 +0100

    MudBlazot test

--- BlazorTimeline/Demo/Program.cs
+++ BlazorTimeline/Demo/Program.cs
@@ -3,13 +3,14 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 
 namespace Demo {
 	public class Program {
 		public static async Task Main(string[] args) {
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
-
+			builder.Services.AddMudServices();
 			builder.Services.AddScoped(
 				_ => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 
